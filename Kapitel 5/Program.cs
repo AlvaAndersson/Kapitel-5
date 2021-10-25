433c1842baf4eb4610e0f9d63cb74d0a364574db
Alva Andersson <04ALANB@skola.lidkoping.se>
2021-10-25 15:57:28 +0200

    hej

--- Kapitel 5/Program.cs
+++ Kapitel 5/Program.cs
@@ -6,7 +6,66 @@ namespace Kapitel_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            string[] svenskaStäder = new string[3];
+
+            svenskaStäder[0] = "Lidköping";
+            svenskaStäder[1] = "Göteborg";
+            Console.WriteLine("Skriv in en valfri stad");
+            svenskaStäder[2] = Console.ReadLine();
+
+            Console.WriteLine($"{svenskaStäder[0]}, {svenskaStäder[1]} och {svenskaStäder[2]}");
+           
+
+            Console.WriteLine("Skriv in antalet sporter du har testat");
+            int anatlSporter = int.Parse(Console.ReadLine());
+            string[] sporter = new string[anatlSporter];
+
+            for (int i = 0; i < anatlSporter; i++)
+            {
+                Console.WriteLine($"Skriv in sport {i + 1} av {anatlSporter}");
+                sporter[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("här kommer alla sporter du har testat");
+
+            for (int i = 0; i < anatlSporter; i++) 
+            {
+                Console.WriteLine(sporter[i]);
+            }
+            
+            int[] tal = { 10, 2, 33, 69, -9898, 33, 16, 8 };
+
+            int max = tal[0];
+
+            for (int i = 1; i < tal.Length; i++)
+            {
+                if (tal[i] > max)
+                {
+                    max = tal[i];
+                }
+            }
+
+            Console.WriteLine($"Det största talet är: {max}");
+            */
+
+            Console.WriteLine("Hej");
+            Console.WriteLine("Skriv din ålder");
+            Console.ReadLine();
+            
+            
+
+            string svar = "";
+            int antal = 0;
+
+            while (svar != "n")
+            {
+
+            }
+
+
+
         }
     }
 }
